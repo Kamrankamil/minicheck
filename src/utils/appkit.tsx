295f2d302e1dbb4259b31acf1438f76bd11c52f2
Kamrankamil <kamrankamil202@gmail.com>
2025-10-25 06:49:18 -0700

    Initial commit for minicheck project

--- src/utils/appkit.tsx
+++ src/utils/appkit.tsx
@@ -24,7 +24,7 @@ if (!projectId) {
 const metadata = {
   name: "Buycex Presale App",
   description: "Powered by Reown AppKit",
-  url: "https://minicheck.vercel.app/",
+  url: "https://minicheck.vercel.app/presale",
   icons: ["https://avatars.githubusercontent.com/u/179229932"]
 }
 
